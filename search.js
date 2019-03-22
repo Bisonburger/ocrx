5d620da39976be1908ab2f69ff3406bd42673293
Young <myoung@medicalsecuritycard.com>
2019-03-22 12:36:49 -0700

    Working async

--- search.js
+++ search.js
@@ -3,9 +3,6 @@
 const request = require('request-promise-native');
 const test_data = require( './test-data.json' );
 
-var probs = [];
-
-
 var formTerms = ( input, window ) => {
   var data = input.split( " " ),
       terms = [],
@@ -18,23 +15,45 @@ var formTerms = ( input, window ) => {
   return terms;
 };
 
-var generateCandidates = (term) =>
-  request.get(`https://rxnav.nlm.nih.gov/REST/approximateTerm.json?term=${term}`)
-    .then( JSON.parse )
-    .then( body => body? body.approximateGroup.candidate : null )
-    .catch();
+var generateCandidates = async term => {
+  let body = null;
+  try{
+    let res = await request.get(`https://rxnav.nlm.nih.gov/REST/approximateTerm.json?term=${term}`);
+    body = JSON.parse( res );
+  }
+  catch( err ){}
+  return body? body.approximateGroup.candidate : null;
+}
 
-var detail = (candidate) =>
-  candidate &&
-  request.get(`https://rxnav.nlm.nih.gov/REST/rxcui/${candidate.rxcui}/properties.json`)
-    .then( JSON.parse )
-    .then( body => body? Object.assign( body.properties, candidate ) : null )
-    .catch();
+var generateDetail = async candidate => {
+  let body = null;
+  try {
+    if( candidate ){
+      let res = await request.get(`https://rxnav.nlm.nih.gov/REST/rxcui/${candidate.rxcui}/properties.json`);
+      body = JSON.parse( res );
+    }
+  }
+  catch( err ){}
+  return body? Object.assign( body.properties, candidate ) : null;
+}
 
-var printDetail = properties => properties && properties.score > 40 && console.log( `Name: ${properties.name}; RXCUI: ${properties.rxcui}; Score: ${properties.score}; Rank: ${properties.rank}`);
+var printDetail = properties => {
+  if( properties ){
+    console.log( `Name: ${properties.name}; RXCUI: ${properties.rxcui}; Score: ${properties.score}; Rank: ${properties.rank}`);
+  }
+}
 
-formTerms( test_data.termComplex, 5 )
-  .forEach( term =>
-              generateCandidates( term )
-                .then( candidates => candidates && candidates.forEach( (c) => detail(c).then( printDetail ) ) )
-                .catch() );
+var start = async () => {
+  let terms = formTerms( test_data.termComplex, 5 );
+  let candidates = await Promise.all( terms.map( generateCandidates ) );
+  candidates = [].concat(...candidates); // flatten the candidates list
+  let details = await Promise.all( candidates.map( generateDetail ) );
+
+  if( details )
+    details = details.filter ( v => v != null )
+               .sort( (a,b) => parseInt( b.score ) - parseInt( a.score ) );
+
+  details && printDetail( details[0] );
+}
+
+start();
